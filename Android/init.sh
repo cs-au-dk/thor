b20817d65ecf5a5342440e8a630c04a45677b0fc
algobardo <mezzetti@gmail.com>
2015-04-10 17:14:28 +0200

    fix

--- Android/init.sh
+++ Android/init.sh
@@ -3,12 +3,12 @@ case "$(uname -s)" in
   Darwin)
 
     # Create platform-tools folder in Android, containing adb (needed by spoon)
-    ln -s /Volumes/Android4.4.3/androidtestingproject/Android/out/host/darwin-x86/bin platform-tools;;
+    ln -s /Volumes/Android4.4.3/thor/Android/out/host/darwin-x86/bin platform-tools;;
 
   Linux)
 
     # Create platform-tools folder in Android, containing adb (needed by spoon)
-    ln -s /Volumes/Android4.4.3/androidtestingproject/Android/out/host/linux-x86/bin platform-tools
+    ln -s /Volumes/Android4.4.3/thor/Android/out/host/linux-x86/bin platform-tools
 
     # Make ack command available
     ln -s /usr/bin/ack-grep /usr/bin/ack;;
