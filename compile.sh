9c16469db37d03818daaf67690daa823b8a3d10b
algobardo <mezzetti@gmail.com>
2015-04-10 15:56:35 +0200

    Better compile

--- compile.sh
+++ compile.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
+THORP="/Volumes/Android4.4.3/thor/"
 echo "Building the Gradle plugin"
 
-(cd GradlePlugin && ./gradlew install)
+(cd ${THORP}GradlePlugin && ./gradlew install)
 
 echo "Building the instrumentation"
 
-(cd Instrumentation/SootAndroidInstrumentation && ./gradlew install)
+(cd ${THORP}Instrumentation/SootAndroidInstrumentation && ./gradlew install)
 
 echo "Building the Espresso wrapper"
 
-(cd Robotium2Espresso && ./gradlew install)
+(cd ${THORP}Robotium2Espresso && ./gradlew install)
 
 echo "Compiling app: AnyMemo"
 
-(cd Applications/AnyMemo && ./gradlew assembleFreeDebug assembleProDebugTest)
+(cd ${THORP}Applications/AnyMemo && ./gradlew assembleFreeDebug assembleProDebugTest)
 
 echo "Compiling app: Car-Cast"
 
-(cd Applications/Car-Cast && ./gradlew assembleDebug assembleDebugTest)
+(cd ${THORP}Applications/Car-Cast && ./gradlew assembleDebug assembleDebugTest)
 
 echo "Compiling app: Numix Calculator"
 
-(cd Applications/com.numix.calculator && ./gradlew assembleDebug assembleDebugTest)
+(cd ${THORP}Applications/com.numix.calculator && ./gradlew assembleDebug assembleDebugTest)
 
 echo "Compiling app: Pocket Code"
 
-(cd Applications/Catroid-latest && ./gradlew assembleDebug assembleDebugTest)
+(cd ${THORP}Applications/Catroid-latest && ./gradlew assembleDebug assembleDebugTest)
 
 echo "Compiling app: Pocket Paint"
 
-(cd Applications/Paintroid && ./gradlew assembleDebug assembleDebugTest)
+(cd ${THORP}Applications/Paintroid && ./gradlew assembleDebug assembleDebugTest)
 
