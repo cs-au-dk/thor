8983721f9b851b98c6ef7d3fc723ad944f8ee5ba
algobardo <mezzetti@gmail.com>
2015-04-10 13:43:05 +0200

    Environment setup

---
+++ env.sh
@@ -0,0 +1,15 @@
+
+case "$(uname -s)" in
+
+Linux)
+	sudo apt-get install cpu-checker
+	if [!$(kvm-ok)]
+		echo "- KVM is not present in your OS installation";
+		exit 1;
+	elif
+	sudo apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
+	echo "Adding the user to the libvirtd group (you need to logout and log back for this to be effective)"
+	sudo adduser `id -un` libvirtd
+	;
+*)
+	echo "Environment set-up is only supported on Ubuntu";
