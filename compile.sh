035b754a54915b79255242ccf5cf732387d6fbbc
christofferqa <christofferqa@gmail.com>
2017-05-17 12:53:56 +0200

    mac osx support

--- compile.sh
+++ compile.sh
@@ -1,35 +1,60 @@
 #!/bin/bash
 
-THORP="/Volumes/Android4.4.3/thor/"
+HOMEFOLDER="/home/$USER"
+THORP="/Volumes/Android4.4.3/thor"
+
+case "$(uname -s)" in
+Linux)
+  ;;
+
+Darwin)
+  HOMEFOLDER="/Users/$USER"
+  ;;
+
+*)
+  echo "Unsupported OS"
+  exit 1
+  ;;
+esac
+
+JAVA_VERSION=`java -version 2>&1`
+
+if [[ $JAVA_VERSION == *"java version \"1.7"* ]]; then
+  echo "Using java 7"
+else
+  echo "java 7 is required"
+  exit 1
+fi
+
 echo "Building the Gradle plugin"
 
-(cd ${THORP}GradlePlugin && ./gradlew install)
+(cd $THORP/GradlePlugin && ./gradlew install)
 
 echo "Building the instrumentation"
 
-(cd ${THORP}Instrumentation/SootAndroidInstrumentation && ./gradlew install)
+(cd $THORP/Instrumentation/SootAndroidInstrumentation && ./gradlew install)
 
 echo "Building the Espresso wrapper"
 
-(cd ${THORP}Robotium2Espresso && ./gradlew install)
+(cd $THORP/Robotium2Espresso && ./gradlew install)
 
 echo "Compiling app: AnyMemo"
 
-(cd ${THORP}Applications/AnyMemo && ./gradlew assembleProDebug assembleProDebugTest)
+(cd $THORP/Applications/AnyMemo && ./gradlew assembleProDebug assembleProDebugTest)
 
 echo "Compiling app: Car-Cast"
 
-(cd ${THORP}Applications/Car-Cast && ./gradlew assembleDebug assembleDebugTest)
+(cd $THORP/Applications/Car-Cast && ./gradlew assembleDebug assembleDebugTest)
 
 echo "Compiling app: Numix Calculator"
 
-(cd ${THORP}Applications/com.numix.calculator && ./gradlew assembleDebug assembleDebugTest)
+(cd $THORP/Applications/com.numix.calculator && ./gradlew assembleDebug assembleDebugTest)
 
 echo "Compiling app: Pocket Code"
 
-(cd ${THORP}Applications/Catroid-latest && ./gradlew assembleDebug assembleDebugTest)
+(cd $THORP/Applications/Catroid-latest && ./gradlew assembleDebug assembleDebugTest)
 
 echo "Compiling app: Pocket Paint"
 
-(cd ${THORP}Applications/Paintroid && ./gradlew assembleDebug assembleDebugTest)
+(cd $THORP/Applications/Paintroid && ./gradlew assembleDebug assembleDebugTest)
 
--- setup.sh
+++ setup.sh
@@ -1,49 +1,107 @@
 #!/bin/bash
 
+COMPRESSION="tgz"
+HOMEFOLDER="/home/$USER"
+OS_NAME="linux"
+
+case "$(uname -s)" in
+Linux)
+  ;;
+
+Darwin)
+  COMPRESSION="zip"
+  HOMEFOLDER="/Users/$USER"
+  OS_NAME="macosx"
+  ;;
+
+*)
+  echo "Unsupported OS"
+  exit 1
+  ;;
+esac
+
+
 read -p "Install dependencies (y/n)?" answer
 case ${answer:0:1} in
   y|Y )
-    sudo apt-get update
-    sudo apt-get -y install openjdk-7-jdk
-    sudo apt-get -y install bison
-    sudo apt-get -y install g++-multilib
-    sudo apt-get -y install gperf
-    sudo apt-get -y install libxml2-utils
-    sudo apt-get -y install make
-    sudo apt-get -y install zlib1g-dev:i386
-    sudo apt-get -y install git zip gradle curl redis-server dpkg-dev nodejs-legacy npm ack-grep
-    sudo apt-get install libgl1-mesa-dev
+    case "$(uname -s)" in
+
+    Linux)
+      sudo apt-get update
+      sudo apt-get -y install openjdk-7-jdk
+      sudo apt-get -y install bison
+      sudo apt-get -y install g++-multilib
+      sudo apt-get -y install gperf
+      sudo apt-get -y install libxml2-utils
+      sudo apt-get -y install make
+      sudo apt-get -y install zlib1g-dev:i386
+      sudo apt-get -y install git zip gradle curl redis-server dpkg-dev nodejs-legacy npm ack-grep
+      sudo apt-get install libgl1-mesa-dev
+      ;;
+
+    Darwin)
+      brew install git # ... incomplete ...
+      ;;
+
+    esac
   ;;
   n|N )
   ;;
 esac
 
+# -----------------------------------
+# -----------------------------------
+# -----------------------------------
+
 echo "Downloading latest Android SDK"
 
-if [ -f "/home/$USER/Downloads/android-sdk_r24.1.2-linux.tgz" ]; then
+if [ -f "$HOMEFOLDER/Downloads/android-sdk_r24.1.2-$OS_NAME.$COMPRESSION" ]; then
   echo "- already there..."
 else
-  curl http://dl.google.com/android/android-sdk_r24.1.2-linux.tgz -o /home/$USER/Downloads/android-sdk_r24.1.2-linux.tgz
+  curl "http://dl.google.com/android/android-sdk_r24.1.2-$OS_NAME.$COMPRESSION" -o "$HOMEFOLDER/Downloads/android-sdk_r24.1.2-$OS_NAME.$COMPRESSION"
 fi
+
+# ---------------------------------
+# ---------------------------------
+# ---------------------------------
 
 echo "Unpacking latest Android SDK"
 
-if [ -d "/home/$USER/android-sdk-linux" ]; then
-  echo "- already there..."
-else
-  mkdir -p /home/$USER/android-sdk-linux
-  tar zxvf /home/$USER/Downloads/android-sdk_r24.1.2-linux.tgz -C /home/$USER/
-fi
+case "$(uname -s)" in
+
+Linux)
+  if [ -d "$HOMEFOLDER/android-sdk-$OS_NAME" ]; then
+    echo "- already there..."
+  else
+    mkdir -p $HOMEFOLDER/android-sdk-$OS_NAME
+    tar zxvf "$HOMEFOLDER/Downloads/android-sdk_r24.1.2-$OS_NAME.$COMPRESSION" -C $HOMEFOLDER/
+  fi
+  ;;
+
+Darwin)
+  if [ -d "$HOMEFOLDER/android-sdk-$OS_NAME" ]; then
+    echo "- already there..."
+  else
+    mkdir -p $HOMEFOLDER/android-sdk-$OS_NAME
+    unzip "$HOMEFOLDER/Downloads/android-sdk_r24.1.2-$OS_NAME.$COMPRESSION" -d $HOMEFOLDER/
+  fi
+  ;;
+
+esac
+
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 read -p "Install Android SDK dependencies (build tools, SDKs, ...) (y/n)?" answer
 
 function install {
-  MATCHING=$(/home/$USER/android-sdk-linux/tools/android list sdk -u --all | grep -m 1 "$1")
+  MATCHING=$($HOMEFOLDER/android-sdk-$OS_NAME/tools/android list sdk -u --all | grep -m 1 "$1")
   echo "Found $MATCHING"
   if [[ $MATCHING =~ (([0-9]*)-.*)$ ]]; then 
     echo "- sdk item number is ${BASH_REMATCH[2]}"
   fi
-  /home/$USER/android-sdk-linux/tools/android update sdk -u -a -t ${BASH_REMATCH[2]}
+  $HOMEFOLDER/android-sdk-$OS_NAME/tools/android update sdk -u -a -t ${BASH_REMATCH[2]}
 }
 
 case ${answer:0:1} in
@@ -53,6 +111,7 @@ case ${answer:0:1} in
     install "Android.* 5.0.1"
     install "Android SDK Build-tools.* 19.1[^\.]*"
     install "Android SDK Build-tools.* 21.1.2"
+    install "Android SDK Platform-tools.* 23.0.1"
     install "Android Support Repository"
     install "Android Support Library"
     install "Google Repository"
@@ -61,21 +120,28 @@ case ${answer:0:1} in
   ;;
 esac
 
+# ---------------------
+# ---------------------
+# ---------------------
+
 echo "Creating SD card"
 
-mkdir -p /home/$USER/android_images
+mkdir -p $HOMEFOLDER/android_images
 
-if [ -f "/home/$USER/android_images/mysd1.img" ]; then
+if [ -f "$HOMEFOLDER/android_images/mysd1.img" ]; then
   echo "- already there..."
 else
-  /home/$USER/android-sdk-linux/tools/mksdcard -l mysd1 1024M /home/$USER/android_images/mysd1.img
+  $HOMEFOLDER/android-sdk-$OS_NAME/tools/mksdcard -l mysd1 1024M $HOMEFOLDER/android_images/mysd1.img
 fi
+
+# -----------------
+# -----------------
+# -----------------
 
 echo "Cloning Thor"
 
 sudo mkdir -p /Volumes/Android4.4.3
 sudo chown -R $USER /Volumes
-
 
 if [ -d "/Volumes/Android4.4.3/thor" ]; then
   if (cd /Volumes/Android4.4.3/thor && ls .git); then
@@ -86,7 +152,23 @@ if [ -d "/Volumes/Android4.4.3/thor" ]; then
     git clone https://github.com/cs-au-dk/thor.git /Volumes/Android4.4.3/thor
   fi
 else
-  git clone https://github.com/cs-au-dk/thor.git /Volumes/Android4.4.3/thor
+  case "$(uname -s)" in
+
+  Linux)
+    git clone https://github.com/cs-au-dk/thor.git /Volumes/Android4.4.3/thor
+    ;;
+
+  Darwin)
+    # Work around permissions in /Volumes
+    sudo mkdir -p /Volumes/Android4.4.3
+    sudo chmod +a "$USER:allow:add_subdirectory,add_file,directory_inherit" /Volumes/Android4.4.3/
+    cd /Volumes/Android4.4.3
+    mkdir thor
+    cd thor
+    git init && git remote add origin https://github.com/cs-au-dk/thor.git && git fetch origin && git reset --hard origin/master
+    ;;
+
+  esac
 fi
 
 echo "Getting test server dependencies"
@@ -98,37 +180,37 @@ echo "Initializing Thor"
 (cd /Volumes/Android4.4.3/thor/Android && sudo ./init.sh)
 
 if [ ! -f "/Volumes/Android4.4.3/thor/Robotium2Espresso/local.properties" ]; then
-  echo "sdk.dir=/home/$USER/android-sdk-linux" >> /Volumes/Android4.4.3/thor/Robotium2Espresso/local.properties
+  echo "sdk.dir=$HOMEFOLDER/android-sdk-$OS_NAME" >> /Volumes/Android4.4.3/thor/Robotium2Espresso/local.properties
 fi
 
 echo "Initializing applications"
 
 if [ ! -f "/Volumes/Android4.4.3/thor/Applications/AnyMemo/local.properties" ]; then
-  echo "sdk.dir=/home/$USER/android-sdk-linux" >> /Volumes/Android4.4.3/thor/Applications/AnyMemo/local.properties
+  echo "sdk.dir=$HOMEFOLDER/android-sdk-$OS_NAME" >> /Volumes/Android4.4.3/thor/Applications/AnyMemo/local.properties
 fi
 
 if [ ! -f "/Volumes/Android4.4.3/thor/Applications/Car-Cast/local.properties" ]; then
-  echo "sdk.dir=/home/$USER/android-sdk-linux" >> /Volumes/Android4.4.3/thor/Applications/Car-Cast/local.properties
+  echo "sdk.dir=$HOMEFOLDER/android-sdk-$OS_NAME" >> /Volumes/Android4.4.3/thor/Applications/Car-Cast/local.properties
 fi
 
 if [ ! -f "/Volumes/Android4.4.3/thor/Applications/com.numix.calculator/local.properties" ]; then
-  echo "sdk.dir=/home/$USER/android-sdk-linux" >> /Volumes/Android4.4.3/thor/Applications/com.numix.calculator/local.properties
+  echo "sdk.dir=$HOMEFOLDER/android-sdk-$OS_NAME" >> /Volumes/Android4.4.3/thor/Applications/com.numix.calculator/local.properties
 fi
 
 if [ ! -f "/Volumes/Android4.4.3/thor/Applications/Catroid-latest/local.properties" ]; then
-  echo "sdk.dir=/home/$USER/android-sdk-linux" >> /Volumes/Android4.4.3/thor/Applications/Catroid-latest/local.properties
+  echo "sdk.dir=$HOMEFOLDER/android-sdk-$OS_NAME" >> /Volumes/Android4.4.3/thor/Applications/Catroid-latest/local.properties
 fi
 
 if [ ! -f "/Volumes/Android4.4.3/thor/Applications/Paintroid/local.properties" ]; then
-  echo "sdk.dir=/home/$USER/android-sdk-linux" >> /Volumes/Android4.4.3/thor/Applications/Paintroid/local.properties
+  echo "sdk.dir=$HOMEFOLDER/android-sdk-$OS_NAME" >> /Volumes/Android4.4.3/thor/Applications/Paintroid/local.properties
 fi
 
 echo "Downloading the instrumented Android images"
 
-if [ -f "/home/$USER/Downloads/android-images.zip" ]; then
+if [ -f "$HOMEFOLDER/Downloads/android-images.zip" ]; then
   echo "- already there..."
 else
-  curl http://brics.dk/thor/files/android-images.zip -o /home/$USER/Downloads/android-images.zip
+  curl http://brics.dk/thor/files/android-images.zip -o $HOMEFOLDER/Downloads/android-images.zip
 fi
 
 if [ -d "/Volumes/Android4.4.3/thor/Android/out/target/product/generic_x86" ]; then
@@ -137,10 +219,10 @@ else
   mkdir -p /Volumes/Android4.4.3/thor/Android/out/target/product/generic_x86
 fi
 
-unzip /home/$USER/Downloads/android-images.zip -d /Volumes/Android4.4.3/thor/Android/out/target/product/generic_x86
+unzip $HOMEFOLDER/Downloads/android-images.zip -d /Volumes/Android4.4.3/thor/Android/out/target/product/generic_x86/
 
 echo "Setting ANDROID_HOME"
 
-echo "export ANDROID_HOME=/Volumes/Android4.4.3/thor/Android" >> /home/$USER/.bashrc
-. /home/$USER/.bashrc
+echo "export ANDROID_HOME=/Volumes/Android4.4.3/thor/Android" >> $HOMEFOLDER/.bashrc
+. $HOMEFOLDER/.bashrc
 
